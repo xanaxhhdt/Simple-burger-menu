51a1bdb9b5e35665d3556424c787ab06138632b8
vladislav <nicochan00@mail.ru>
2022-07-03 01:44:16 +0300

    ending poject

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const humb = document.querySelector('#hamb');
+const poppup = document.querySelector('#poppup');
+const menu = document.querySelector('.menu').cloneNode(1);
+const body = document.body;
+
+humb.addEventListener('click', humbHandler);
+
+function humbHandler(event) {
+   event.preventDefault();
+   
+   poppup.classList.toggle('open');
+   humb.classList.toggle('active'); 
+   body.classList.toggle('noscroll');
+   renderPoppup();
+}
+
+function renderPoppup() {
+   poppup.append(menu);
+}
